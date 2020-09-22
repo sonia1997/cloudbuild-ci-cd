a5757e55afe0741aa518f3459cccf8a054d63cd7
sonia1997 <pandita.sonia98@gmail.com>
2020-09-22 20:35:46 +0530

    Added many env varaibles

--- app.py
+++ app.py
@@ -10,7 +10,15 @@ def Main():
 @app.route('/projectname')
 def getProjectName():
     project_name=os.environ.get('PROJECT_NAME')
-    response={"Project_name": project_name, "User name": "Sonia"}
+    build=os.environ.get('BUILD')
+    tag=os.environ.get('tag')
+    repo=os.environ.get('repo')
+    branch=os.environ.get('branch')
+    short=os.environ.get('short')
+    rev=os.environ.get('REV')
+    commit=os.environ.get('commit')
+
+    response={"Project_name": project_name, "Build": build, "Branch": branch, "Commit": commit, "Tag": tag, "Repo": repo, "Short": short}
     return jsonify(response)
 
 if __name__ == "__main__":
