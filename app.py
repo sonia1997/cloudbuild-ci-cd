698980c6bd404227dc9c80c72f19d403719d378e
sonia1997 <pandita.sonia98@gmail.com>
2020-09-22 19:59:08 +0530

    Added a route for project name.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app=Flask(__name__)
 @app.route('/')
 def Main():
     project_id=os.environ.get('PROJECT_ID')
     return render_template('project_id.html', project_id=project_id)
+
+@app.route('/projectname')
+def getProjectName():
+    project_name=os.environ.get('PROJECT_NAME')
+    response={"Project_name": project_name}
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run()
